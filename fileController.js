8e40c240fdd98eb4c78a8fd41abf546f1cd2e282
jungjuyoung <nwwn1004@naver.com>
2021-07-01 17:29:30 +0900

    style: annotation description

--- fileController.js
+++ fileController.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import { resolve } from 'path';
 
-const basePath = resolve();
+const basePath = resolve(); // 기본 베이스 절대경로
 
 export const readJSON = (path) =>
   JSON.parse(fs.readFileSync(resolve(basePath, path), 'utf-8'));
